bbde1c0904223717116dc3e89e7797a32cb23a3f
Carlos Zurita <carloszman43@gmail.com>
2019-06-07 19:10:15 -0500

    Tables Sides and GameModes added

--- HeavensLadder.sql
+++ HeavensLadder.sql
@@ -16,3 +16,21 @@ create table [Team](
     teamname varchar(30) not null
 )
 go
+
+create table Sides(
+    id int PRIMARY KEY IDENTITY,
+    challengeid int not null,
+    FOREIGN KEY (challengeid) REFERENCES [Challenge](id),
+    teamid int not null,
+    FOREIGN KEY (teamid) REFERENCES [Team](id),
+    winreport bit not null
+)
+
+create table GameModes(
+    id int PRIMARY KEY IDENTITY,
+    modename varchar(30) not null
+)
+go
+
+select * from GameModes
+select * from Sides
