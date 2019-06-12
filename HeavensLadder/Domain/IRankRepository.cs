9ff03e47ccff88971b8a850070a6a957428f2947
Edwin De La Sierra <edwin.delasierra@yahoo.com>
2019-06-12 18:43:00 -0500

    Added IRankRepository interface

---
+++ HeavensLadder/Domain/IRankRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public interface IRankRepository
+    {
+        List<Rank> GetAllRanks();
+        List<Rank> GetRanksByMode(string gamemode);
+        List<Rank> GetRanksByTeam(Team team);
+        List<Rank> GetRank(Team team, string gamemode);
+    }
+}
