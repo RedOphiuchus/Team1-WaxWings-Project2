6238fa2f3f26bbad01ef3a70a885044c028147d3
Akash <akashmukeshpatel@outlook.com>
2019-06-12 18:52:29 -0700

    added defintiions for repository methods

---
+++ HeavensLadder/Data/TeamRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+
+namespace Data
+{
+    class TeamRepository : ITeamRepository
+    {
+        private readonly Entities.HLContext _db;
+        public TeamRepository(Entities.HLContext db)
+        {
+            _db = db;
+        }
+
+        public bool AddTeam(Team team)
+        {
+            bool success = false;
+           // _db.Team.Add(Mapper.Map(team));
+            return success;
+        }
+
+        public bool DeleteTeam(Team team)
+        {
+            bool success = false;
+            return success;
+        }
+        public bool UpdateTeam(Team team)
+        {
+            bool success = false;
+            return success;
+        }
+        public List<Team> GetUserTeams(User user)
+        {
+            List<Team> x = new List<Team>();
+            return x;
+        }
+
+        public Team GetByTeamName(string name)
+        {
+            Team something = new Team();
+            return something;
+        }
+
+
+
+    }
+}
---
+++ HeavensLadder/Domain/ITeamRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public interface ITeamRepository
+    {
+        bool AddTeam(Team team);
+        bool DeleteTeam(Team team);
+        bool UpdateTeam(Team team);
+        //List<Team> GetChallengeTeams(Challenge chal);
+        List<Team> GetUserTeams(User user);
+        Team GetByTeamName(string name);
+
+    }
+}
