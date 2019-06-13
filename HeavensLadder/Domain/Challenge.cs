a57027a40a0dba2b66441dd74e1d40a93aaf2d96
Sera <005185709@coyote.csusb.edu>
2019-06-13 10:24:44 -0500

    Added the domain class for Challenge.

--- HeavensLadder/Domain/Challenge.cs
+++ HeavensLadder/Domain/Challenge.cs
@@ -23,5 +23,43 @@ namespace Domain
             Team2 = team2;
             GameMode = gameMode;
         }
+
+        Challenge(int id, Team team1, Team team2, string gameMode, bool? team1Report, bool? team2Report)
+        {
+            this.id = id;
+            Team1 = team1;
+            Team2 = team2;
+            GameMode = gameMode;
+            Team1Report = team1Report;
+            Team2Report = team2Report;
+        }
+
+        bool? Victor()
+        {
+            if (Team1Report == null || Team2Report == null)
+                return null;
+
+            if (Team1Report == true && Team2Report == false)
+                return true;
+            if (Team1Report == false && Team2Report == true)
+                return false;
+
+            return null;
+        }
+
+        bool MakeReport(Team team, bool result)
+        {
+            if(team == Team1)
+            {
+                Team1Report = result;
+                return true;
+            }
+            if(team == Team2)
+            {
+                Team2Report = result;
+                return true;
+            }
+            return false;
+        }
     }
 }
