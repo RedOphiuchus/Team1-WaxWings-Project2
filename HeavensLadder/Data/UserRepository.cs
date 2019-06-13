6ce8da804ef8016fbb886dce65ea61529b670ff5
Carlos Zurita <carloszman43@gmail.com>
2019-06-13 14:30:39 -0500

    UserRepoTest update

--- HeavensLadder/Data/UserRepository.cs
+++ HeavensLadder/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 using Data.Entities;
 using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Text;
 
 namespace Data
 {
-    class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository
     {
         private readonly HLContext _db;
         public UserRepository(HLContext db)
@@ -18,10 +19,24 @@ namespace Data
         {
             return _db.User.Select(x => Mapper.Map(x));
         }
+        public Domain.User GetUserByUserid(int userid)
+        {
+            var element = _db.User.Where(a => a.Id == userid).FirstOrDefault();
+            if (element != null)
+                return Mapper.Map(element);
+            else
+                return null;
+        }
         public bool AddUser(Domain.User user)
         {
             bool check = false;
             _db.User.Add(Mapper.Map(user));
+            return check;
+        }
+        public bool DeleteUser(Domain.User user)
+        {
+            bool check = false;
+            _db.User.Remove(_db.User.Find(user.username));
             return check;
         }
         public bool validatelogin(string username, string password)
@@ -49,8 +64,18 @@ namespace Data
             List<Domain.User> usersinteam = new List<Domain.User>();
             var teamwanted = _db.Team.Where(a => a.Teamname == teamname).FirstOrDefault();
             var teamwantedid = teamwanted.Id;
+            var usersteaminteam = _db.UserTeam.Where(a => a.Teamid == teamwantedid).Include("User");
+
+            foreach (var userteaminteam in usersteaminteam)
+            {
+                usersinteam.Add(GetUserByUserid(userteaminteam.Userid));
+            }
 
             return usersinteam;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
         }
     }
 }
--- HeavensLadder/Domain/IUserRepository.cs
+++ HeavensLadder/Domain/IUserRepository.cs
@@ -7,9 +7,12 @@ namespace Domain
     public interface IUserRepository
     {
         IEnumerable<Domain.User> GetUsers();
+        User GetUserByUserid(int userid);
         bool AddUser(Domain.User user);
+        bool DeleteUser(Domain.User user);
         bool validatelogin(string username, string password);
         bool validateusername(string username);
         List<Domain.User> TeamUsers(string teamname);
+        void Save();
     }
 }
