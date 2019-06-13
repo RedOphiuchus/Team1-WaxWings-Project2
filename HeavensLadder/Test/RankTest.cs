ed72c19be192d69f39e68702069eadbe354d2da0
Edwin De La Sierra <edwin.delasierra@yahoo.com>
2019-06-12 19:50:20 -0500

    Added test cases for Rank class

---
+++ HeavensLadder/Test/RankTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    [TestClass]
+    class RankTest
+    {
+        [TestMethod]
+        public void TestRankConstructor()
+        {
+            User user = new User("testuser", "pw");
+            Team team = new Team(user);
+        }
+    }
+}
