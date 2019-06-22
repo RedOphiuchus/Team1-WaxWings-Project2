63c43941d696bd23960104e5d07de0c2a62b2a94
Carlos Zurita <carloszman43@gmail.com>
2019-06-22 16:09:58 -0500

    added teams that a user belongs to

--- HeavensLadder/WebAPI/Controllers/UserController.cs
+++ HeavensLadder/WebAPI/Controllers/UserController.cs
@@ -15,8 +15,10 @@ namespace WebAPI.Controllers
     {
         //intialize repository objects so that we can use the repository methods
         private readonly IUserRepository _UserRepository;
-        public UserController(IUserRepository UserRepository)
+        private readonly ITeamRepository _TeamRepository;
+        public UserController(ITeamRepository TeamRepository, IUserRepository UserRepository)
         {
+            _TeamRepository = TeamRepository;
             _UserRepository = UserRepository;
         }
 
@@ -26,7 +28,22 @@ namespace WebAPI.Controllers
         //{
         //    return _UserRepository.validateusername(username);
         //}
+        // GET list of strings of teams that a user is in
+        [HttpGet("Teams")]
+        public IActionResult UsersInTeam(string username)
+        {
+            List<string> TeamNameList = new List<string>();
+            List<Domain.Team> TeamsList = _TeamRepository.GetUserTeams(_UserRepository.GetUserByUsername(username));
 
+            if (TeamsList.Count == 0)
+                return BadRequest(new { message = "This user doesn't have any teams." });
+            foreach (var TeamList in TeamsList)
+            {
+                TeamNameList.Add(TeamList.teamname);
+            }
+
+            return Ok(TeamNameList);
+        }
         // POST boolean for login
         [HttpPost("Login")]
         //public IActionResult Login([FromBody] Domain.User user)
